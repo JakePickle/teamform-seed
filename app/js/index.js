c1a93627847f701ea2fe1eab0f2b8dac9ca99dae
Jakob Liu <jacoblxliu@gmail.com>
2016-11-02 18:08:53 +0800

    test161102

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,37 @@
 $(document).ready(function(){
+  var w = screen.availWidth;
+  var h = screen.availHeight;
+  var ww = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
 
+  var wh = window.innerHeight
+    || document.documentElement.clientHeight
+    || document.body.clientHeight;
 
-    $("#btn_admin").click(function(){
+  var h1h = $(".headline h1").outerHeight(true);
+  var ph = $(".headline p").outerHeight(true);
+  var navh = $(".navbar").innerHeight();
+  //$(".carousel img").css("height", h);
+  //$(".carousel img").css("width", w);
+  $(".headline h1").css("padding-top", wh/2-h1h-navh/2-ph);
+  $(".headline").css("height", h-navh);
+  $(".buttonline").css("height", h-navh);
+  $(".transparent").css("height", navh);
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > wh/2-h1h-navh/2-ph) {
+      $(".navbar").css("background","black");
+    }
+    else {
+      $(".navbar").css("background","rgba(0, 0, 0, 0.2)");
+    }
+  });
+  $(".buttonblock").css("padding-top", wh/2-h1h-navh/2-ph);
+  var tw = $(".tuto").width();
+  var th = $(".tuto").height();
+  $("iframe").attr("width", 0.8*tw).attr("height", 0.45*tw);
+  $(".fill").css("height", $(".tuto").outerHeight());
+/*    $("#btn_admin").click(function(){
     	var val = $('#input_text').val();
     	if ( val !== '' ) {
     		var url = "admin.html?q=" + val;
@@ -27,6 +57,6 @@ $(document).ready(function(){
     		return false;
     	}
     });
-
+*/
 
 });
