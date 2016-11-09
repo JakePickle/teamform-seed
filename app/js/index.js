f3b41e0112f143d03b4b442de661bf091900d4d1
Jacob Pickle <jmp6xb@mst.edu>
2016-11-09 21:04:34 +0800

    testing username get

--- app/js/index.js
+++ app/js/index.js
@@ -14,11 +14,40 @@ angular.module('teamform-index-app', ['firebase'])
 
     initalizeFirebase();
 
+    $scope.userName = "";
+
     $scope.authenticate = function()
     {
         var provider = new firebase.auth.GoogleAuthProvider();
         provider.addScope('https://www.googleapis.com/auth/plus.login');
         firebase.auth().signInWithRedirect(provider);
+
+        firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+            // ...
+            }
+            // The signed-in user info.
+            var user = result.user;
+            var name, email, photoUrl, uid;
+
+            if (user != null) {
+                name = user.displayName;
+                email = user.email;
+                photoUrl = user.photoURL;
+                uid = user.uid;  
+            }
+
+            $scope.userName = name;
+
+        }).catch(function(error) {
+            //TODO: Handel Errors
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            var email = error.email;
+            var credential = error.credential;
+        });
     }
 
 }]);
