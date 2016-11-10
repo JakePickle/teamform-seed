79b07c517593b0604c381d3e20d3a407c79940f7
csysuao_apmapple <suyanzhe970604@outlook.com>
2016-11-10 21:54:55 +0800

    Remove duplicated navbar code, use angular routing instead. Fix mobile navbar problem (#35)

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -2,7 +2,7 @@ var navh, sh, sideNavh, sideNavAh;
 //Formatting with jQuery
 $(window).resize(function() {
     navh = $(".navbar").innerHeight();
-    sh = $(window).height();//screen.availHeight;
+    sh = $(window).height();
     sideNavh = (sh - navh) / 5;
     sideNavAh = $(".sideNav a:last").height();
     $(".setMargin").css("margin-top", navh);
@@ -12,3 +12,62 @@ $(window).resize(function() {
     $(".sideNav a").css("padding-top", (sideNavh - sideNavAh) / 2);
 });
 $(window).trigger('resize');
+var app = angular.module("teamform", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "dashboard.html"
+        })
+        .when("/team", {
+            templateUrl: "team.html"
+        })
+        .when("/event", {
+            templateUrl: "event.html"
+        })
+        .when("/explore", {
+            templateUrl: "explore.html"
+        })
+        .when("/message", {
+            templateUrl: "message.html"
+        })
+        .when("/editProfile", {
+            templateUrl: "profileForm.html",
+            controller: "updateButtonCtrl"
+        })
+        .when("/createProfile", {
+            templateUrl: "profileForm.html",
+            controller: "startCtrl"
+        })
+});
+app.controller("updateButtonCtrl", function($scope) {
+    $scope.text = "updateProfileView!";
+});
+app.controller("startCtrl", function($scope) {
+    $scope.text = "Let's start!";
+});
+function switchPage(page) {
+    $("#main .sideNav").removeClass("active");
+    switch (page) {
+        case "team.html":
+            $("#team").addClass("active");
+            break;
+        case "event.html":
+            $("#event").addClass("active");
+            break;
+        case "explore.html":
+            $("#explore").addClass("active");
+            break;
+        case "message.html":
+            $("#message").addClass("active");
+            break;
+    }
+};
+function getQueryVariable(param) {
+    console.log(window.location.search);
+    var params = window.location.search.substring(1).split("&");
+    for (var i = 0; i < params.length; i++) {
+        var pair = params[i].split("=");
+        if (pair[0] == param) return pair[1];
+    }
+    return false; // Not found
+}
