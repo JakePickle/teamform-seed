633478f4d06ba34c8091eaf79e0d1923778dccb7
Jacob Pickle <jmp6xb@mst.edu>
2016-11-10 21:44:09 +0800

    profile connects to firebase?

---
+++ app/js/profile.js
@@ -0,0 +1,56 @@
+angular.module('teamform-profile-app', ['firebase'])
+.controller('ProfileCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
+
+    initalizeFirebase();
+
+    $scope.uid = "";
+    $scope.username = "";
+
+    $scope.getUID = function()
+    {
+       firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+            // ...
+            }
+            // The signed-in user info.
+            user = result.user;
+            if (user != null) {
+                $scope.uid = user.uid;
+            }
+
+
+
+        }).catch(function(error) {
+            //TODO: Handel Errors
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            var email = error.email;
+            var credential = error.credential;
+        }); 
+    }
+
+    $scope.loadFunc = function() {
+        $scope.getUID();
+        var userID = $scope.uid;
+        if ( userID !== '' ) {
+            
+            var refPath = "Users/" + getURLParameter("q") $scope.uid;
+            retrieveOnceFirebase(firebase, refPath, function(data) {
+                                
+                if ( data.child("name").val() != null ) {
+                    $scope.username = data.child("name").val();
+                } else {
+                    $scope.username = "";
+                }
+                $scope.$apply();
+            });
+        }
+    }
+
+    
+
+    
+
+}]);
