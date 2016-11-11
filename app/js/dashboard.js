4dc116eb24adc8b32030ce9e18401cbee5ddbc65
csysuao_apmapple <suyanzhe970604@outlook.com>
2016-11-11 12:43:50 +0800

    Revert "Prevent the side nav from being too crowded"
    This reverts commit d7145031247c047c7bebedea832bb53dd10aeaf5.

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -3,11 +3,10 @@ var navh, sh, sideNavh, sideNavAh;
 $(window).resize(function() {
     navh = $(".navbar").innerHeight();
     sh = $(window).height();
-    var remainH = Math.max(sh - navh, 512);
-    sideNavh = remainH / 5;
+    sideNavh = (sh - navh) / 5;
     sideNavAh = $(".sideNav a:last").height();
     $(".setMargin").css("margin-top", navh);
-    $(".setMargin").css("height", remainH);
+    $(".setMargin").css("height", sh - navh);
     $(".sideNav").css("height", sideNavh);
     $("#profilePic a").css("height", sideNavh);
     $(".sideNav a").css("padding-top", (sideNavh - sideNavAh) / 2);
