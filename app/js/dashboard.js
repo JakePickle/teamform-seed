ba39a9982d6b1c66ee60971a2eea96fd4ce6bbca
csysuao_apmapple <suyanzhe970604@outlook.com>
2016-11-11 13:20:13 +0800

    Fix issues #31 & #35

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -1,19 +1,25 @@
-var navh = $(".navbar").innerHeight();
-var sh = screen.availHeight;
-var sideNavh = (sh-navh)/5;
-var sideNavAh = $(".sideNav a:last").height();
+var navh, sh, sideNavh, sideNavAh;
+$(window).resize(function () {
+    navh = $(".navbar").innerHeight();
+    sh = $(window).height();
+    var remainH = Math.max(sh - navh, 512);
+    sideNavh = remainH / 5;
+    sideNavAh = $(".sideNav a:last").height();
+    $(".setMargin").css("margin-top", navh);
+    $(".setMargin").css("height", remainH);
+    $(".sideNav").css("height", sideNavh);
+    $("#profilePic a").css("height", sideNavh);
+    $(".sideNav a").css("padding-top", (sideNavh - sideNavAh) / 2);
+});
+$(window).trigger('resize');
 
-$(document).ready(function(){
+$(document).ready(function () {
     //.updateProfileView class hidden at first
     $(".updateProfileView").hide();
     //Formatting with jQuery
-    $(".setMargin").css("margin-top", navh);
-    $(".setMargin").css("height", sh-navh);
-    $(".sideNav").css("height", sideNavh);
-    $("#profilePic a").css("height", sideNavh);
-    $(".sideNav a").css("padding-top", (sideNavh-sideNavAh)/2);
+    $(window).trigger('resize');
     //"Update Profile" button click event will toggle the view
-    $("#updateProfile").click(function(){
+    $("#updateProfile").click(function () {
         $(".profileView").hide();
         $(".updateProfileView").show();
     });
