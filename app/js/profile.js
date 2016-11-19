e47720bcfa4376980059746d0a36c6ce09c933b0
Jacob Pickle <jmp6xb@mst.edu>
2016-11-19 08:04:24 +0800

    Cleaned up profile

--- app/js/profile.js
+++ app/js/profile.js
@@ -1,50 +1,14 @@
 angular.module('teamform-profile-app', ['firebase'])
-.controller('ProfileCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$interval' ,'$timeout', function($scope, $firebaseObject, $firebaseArray, $interval, $timeout) {
+.controller('ProfileCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
 
     initalizeFirebase();
 
     $scope.uid = "";
     $scope.username = "";
 
-    $scope.authenticate = function()
-    {
-        var provider = new firebase.auth.GoogleAuthProvider();
-        provider.addScope('https://www.googleapis.com/auth/plus.login');
-        firebase.auth().signInWithRedirect(provider);
-    }
-
-    $scope.getUID = function()
-    {
-        firebase.auth().getRedirectResult().then(function(result) {
-        if (result.credential) {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-                var token = result.credential.accessToken;
-            // ...
-            }
-            // The signed-in user info.
-            user = result.user;
-            if (user != null) {
-                $scope.uid = user.uid;
-            }
-
-
-
-        }).catch(function(error) {
-            //TODO: Handel Errors
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            var email = error.email;
-            var credential = error.credential;
-        }); 
-    }
-
-    $scope.loadFuncHelper = function() {
-        //$scope.getUID();
-        if($scope.uid == "")
-        {      
-            //$scope.authenticate();
-        }else{
-            
+    $scope.loadFunc = function() {
+        if($scope.uid != "")
+        {
             var refPath = "Users/" + getURLParameter("q") + $scope.uid;
             retrieveOnceFirebase(firebase, refPath, function(data) {
                                 
@@ -58,25 +22,21 @@ angular.module('teamform-profile-app', ['firebase'])
         }  
     }
 
-    $scope.loadFunc = function() {
-        //$interval($scope.loadFuncHelper(), 2000, 10);
-        //$timeout($scope.loadFuncHelper(), 11000);
-        $scope.uid = getCookie("uid");
-        $scope.loadFuncHelper();
+    function getCookie(cname) { //Copied from w3schools.com
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
     }
 
-    function getCookie(cname) { //Copied from w3schools.com
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
+    $scope.uid = getCookie("uid");
+    $scope.loadFunc();
 }]);
