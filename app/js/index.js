957674b96a652c406f47508717fa0e8f39d598e7
Jacob Pickle <JakePickle@users.noreply.github.com>
2016-11-23 01:36:58 +0800

    Google Authorization Implementation (#90)
    * added authenticate() method
    
    * testing username get
    
    * forgot some stuff
    
    * more testing
    
    * capital letter mistake
    
    * same mistake
    
    * trying things
    
    * ok now?
    
    * call getInfo()
    
    * not sure what the problem is
    
    * probably that
    
    * twice?
    
    * I don't know why I thought that would work
    
    * Firebase conectivity?
    
    * redirect to profile after saveFunc
    
    * profile connects to firebase?
    
    * profile calls loadFunc()
    
    * btn calls loadFunc()
    
    * include profile.js
    
    * now include profile.js
    
    * not sure what the problem is
    
    * Not sure how I missed that.
    
    * now authenticate
    
    * Ugly while loop
    
    * added sleep
    
    * longer sleep
    
    * changed sleep to timeout
    
    * semi problems
    
    * changed timeout to interval
    
    * setInterval?
    
    * I am an idiot
    
    * more test
    
    * added loadFuncHelper()
    
    * authenticate on page load
    
    * that infinite loops....... :boom: :frowning:
    
    * still loops :boom: :boom: :boom:
    
    * include interval
    
    * longer delay
    
    * interval is now timeout
    
    * I keep forgeting this
    
    * forgot $
    
    * bad idea
    
    * maybe now
    
    * lets try this
    
    * angularjs version of timeout
    
    * I accidentally a number
    
    * trying things
    
    * test output
    
    * better test output
    
    * more testing
    
    * call getUID()
    
    * authenticate
    
    * only get ID
    
    * blah
    
    * even simpler test
    
    * type="button"
    
    * now loadFunc()
    
    * testing cookies
    
    * loadFuncHelper call
    
    * simpler cookie test
    
    * ignore this
    
    * less simple cookie test
    
    * Cleaned up profile
    
    * save more info in database
    
    * I did that wrong, now its saved in the database
    
    * added email to profile
    
    * attempt to fix jumping to profile page when authenticated
    
    * trying to fix error
    
    * that didn't work
    
    * Spelling Error Corrected
    
    * It was spelled wrong in profile aswell
    
    * This wasn't working
    
    * forgot some letters
    
    * this lets unique identifier be uid
    
    * Remove duplicate form groups
    
    Hopefully I kept the right ones
    
    * uid was out of scope
    
    * now user angular
    
    * miss copied old code
    
    * didn't change a variable
    
    * Back to index
    
    * moved getCookie() to site.js
    
    * authAttempt cookie :cookie:
    
    * more testing
    
    * ok now try this
    
    * added sleep
    
    * hope this works
    
    * implemented asyncHelper()
    
    * maybe this
    
    * still got a problem
    
    * at this point I don't even think this will work
    
    * Some ugly code
    
    * well, it worked
    
    * Removed button used for debugging
    
    * Removed Debug Text box
    
    * should now link to dashboard after profile creation

--- app/js/index.js
+++ app/js/index.js
@@ -10,15 +10,87 @@ $(document).ready(function(){
 });
 
 angular.module('teamform-index-app', ['firebase'])
-.controller('IndexCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$interval', function($scope, $firebaseObject, $firebaseArray, $interval) {
+.controller('IndexCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '$interval', '$timeout', function($scope, $firebaseObject, $firebaseArray, $interval, $timeout) {
 
     initializeFirebase();
+
+    var user;
+    $scope.username = "";
+    $scope.email = "";
+    $scope.photoUrl = "";
+    $scope.uid = "";
 
     $scope.authenticate = function()
     {
         var provider = new firebase.auth.GoogleAuthProvider();
         provider.addScope('https://www.googleapis.com/auth/plus.login');
+        document.cookie = "authAttempt=1";
         firebase.auth().signInWithRedirect(provider);
+    }
+
+    $scope.getInfo = function()
+    {
+        firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+            // ...
+            }
+            // The signed-in user info.
+            user = result.user;
+            if (user != null) {
+                $scope.username = user.displayName;
+                $scope.email = user.email;
+                $scope.photoUrl = user.photoURL;
+                $scope.uid = user.uid;
+                document.cookie = "uid="+user.uid;
+            }
+
+            $scope.saveFunc();
+
+
+
+        }).catch(function(error) {
+            //TODO: Handel Errors
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            var email = error.email;
+            var credential = error.credential;
+        });     
+    }
+
+    $scope.saveFunc = function() {
+        
+        var userName = $.trim( $scope.username );
+        var Email = $.trim( $scope.email );
+        var PhotoUrl = $.trim( $scope.photoUrl );
+        
+        if (userName !== '') {
+                                    
+            var newData = {             
+                'name': userName,
+                'email': Email,
+                'photoUrl': PhotoUrl
+            };
+            
+            var refPath = "Users/" + getURLParameter("q") + $scope.uid;  
+            var ref = firebase.database().ref(refPath);
+            
+            ref.set(newData, function(){
+                // complete call back
+                //alert("data pushed...");
+                
+                // Finally, go back to the front-end
+                window.location.href= "profile.html";
+            });
+
+        }
+    }
+
+    if(getCookie("authAttempt")!="")
+    {
+        $interval($scope.getInfo(), 1000, 4);
+        $scope.saveFunc();
     }
 
 }]);
