7bed34bb9fd96bf48ef3150cc23c3775419543f5
Jacob Pickle <jmp6xb@mst.edu>
2016-11-26 14:05:38 +0800

    Profile is broken :frowning: :angry:

--- app/js/profile.js
+++ app/js/profile.js
@@ -7,6 +7,9 @@ profileApp.controller('ProfileCtrl', ['$scope', '$firebaseObject', '$firebaseArr
     $scope.uid = "";
     $scope.username = "";
     $scope.email = "";
+    $scope.languages;
+    $scope.country;
+    $scope.city;
 
     $scope.loadFunc = function() {
         if($scope.uid != "")
@@ -95,9 +98,12 @@ profileApp.controller('ProfileCtrl', ['$scope', '$firebaseObject', '$firebaseArr
         form.Email = $('#email').val();
         form.Gender = $('input[name=gender]:checked').val();
         form.Birthday = new Date($('#birthday').val()).getTime();
-        form.Languages = $('#language').val().split(/\W+/).filter(Boolean);
-        form.Country = $('input[name=country]').val();
-        form.City = $('input[name=city]').val();
+        //form.Languages = $('#language').val().split(/\W+/).filter(Boolean);//deprecated
+        form.Languages = $scope.languages;
+        //form.Country = $('input[name=country]').val();//deprecated
+        //form.City = $('input[name=city]').val();//deprecated
+        form.country = $scope.country;
+        form.city = $scope.city;
         form.Education = $('input[name=education]:checked').val();
         form.Skills = $('#skill').val().split(/[^\w+|C\+\+]/).filter(Boolean);
         form.Introduction = $('#introduction').val();
